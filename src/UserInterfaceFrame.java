a76e070752cf5ec903958ff3708d0f02fac07080
hellyanalves <hellyanalves@gmail.com>
2017-07-02 12:11:27 -0300

    Interface - ação do botão (Ainda não chamando programa para processamento)

--- src/UserInterfaceFrame.java
+++ src/UserInterfaceFrame.java
@@ -1,10 +1,20 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;import javax.swing.event.ListDataListener;
+import javax.swing.plaf.LabelUI;
 import javax.swing.ComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dialog;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.text.MessageFormat;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class UserInterfaceFrame {
 
@@ -42,39 +52,60 @@ public class UserInterfaceFrame {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+
 		JButton btnMostrar = new JButton("Mostrar");
-		btnMostrar.setBounds(103, 210, 117, 25);
+		btnMostrar.setBounds(12, 210, 117, 25);
 		frame.getContentPane().add(btnMostrar);
 		
 		JComboBox cmbVariavel = new JComboBox(Variavel.values());
-		cmbVariavel.setBounds(103, 46, 117, 24);
+		cmbVariavel.setBounds(12, 46, 117, 24);
 		frame.getContentPane().add(cmbVariavel);
 		
 		JComboBox<Integer> cmbAnoIni = new JComboBox<Integer>();
-		cmbAnoIni.setBounds(103, 101, 58, 24);
+		cmbAnoIni.setBounds(12, 101, 58, 24);
 		populateComboBoxAnos(cmbAnoIni);
 		frame.getContentPane().add(cmbAnoIni);
 		
 		JComboBox<Integer> cmbAnoFinal = new JComboBox<Integer>();
-		cmbAnoFinal.setBounds(162, 101, 58, 24);
+		cmbAnoFinal.setBounds(71, 101, 58, 24);
 		populateComboBoxAnos(cmbAnoFinal);
 		frame.getContentPane().add(cmbAnoFinal);
 		
 		JLabel lblVarivel = new JLabel("Variável");
-		lblVarivel.setBounds(103, 30, 70, 15);
+		lblVarivel.setLabelFor(cmbVariavel);
+		lblVarivel.setBounds(12, 30, 70, 15);
 		frame.getContentPane().add(lblVarivel);
 		
 		JLabel lblPerodo = new JLabel("Período");
-		lblPerodo.setBounds(103, 82, 70, 15);
+		lblPerodo.setBounds(12, 82, 70, 15);
 		frame.getContentPane().add(lblPerodo);
 		
 		JLabel lblAgruparPor = new JLabel("Agrupar por:");
-		lblAgruparPor.setBounds(103, 137, 117, 15);
+		lblAgruparPor.setBounds(12, 137, 117, 15);
 		frame.getContentPane().add(lblAgruparPor);
 		
 		JComboBox cmbAgrupamento = new JComboBox(Agrupamento.values());
-		cmbAgrupamento.setBounds(103, 164, 117, 24);
+		lblAgruparPor.setLabelFor(cmbAgrupamento);
+		cmbAgrupamento.setBounds(12, 164, 117, 24);
 		frame.getContentPane().add(cmbAgrupamento);
+		
+		JPanel pnlGrafico = new JPanel();
+		pnlGrafico.setBackground(Color.GRAY);
+		pnlGrafico.setBounds(147, 46, 276, 189);
+		frame.getContentPane().add(pnlGrafico);
+		
+		JLabel lblLblvariaveis = new JLabel("");
+		pnlGrafico.add(lblLblvariaveis);
+		
+		btnMostrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String sPeriodo = MessageFormat.format("{0}-{1}", cmbAnoIni.getSelectedItem().toString(), cmbAnoFinal.getSelectedItem().toString());
+				Variavel var = (Variavel) cmbVariavel.getSelectedItem();
+				Agrupamento agrupamento = (Agrupamento) cmbAgrupamento.getSelectedItem();
+				lblLblvariaveis.setText(MessageFormat.format("{0} {1} {2}",
+						var.toString(), sPeriodo, agrupamento.toString()));
+			}
+		});
 	}
 	void populateComboBoxAnos(JComboBox<Integer> jcbbAno) {
 		for(int i=1900;i<2018;i++) {
